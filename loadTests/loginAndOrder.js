10bfc60374b60381761d1ee453de408c53a008b2
GuernJaq <guernjaq000@gmail.com>
2024-11-19 00:11:28 -0700

    #d10 added loginAndOrder.js

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,183 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+
+  group('page_1 - https://pizza.boospace.click/', function () {
+    // Homepage
+    response = http.get('https://pizza.boospace.click/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        dnt: '1',
+        'if-modified-since': 'Tue, 29 Oct 2024 21:30:22 GMT',
+        'if-none-match': '"d63b37155c5471a17e1b96d3c57fa394"',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(10)
+
+    // Login
+    response = http.put(
+      'https://pizza-service.boospace.click/api/auth',
+      '{"email":"guernjaq000@gmail.com","password":"Pytm2_JF86WPNW5"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          dnt: '1',
+          origin: 'https://pizza.boospace.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+
+    vars['token'] = jsonpath.query(response.json(), '$.token')[0]
+
+    sleep(5)
+
+    // Get Menu
+    response = http.get('https://pizza-service.boospace.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        dnt: '1',
+        'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+        origin: 'https://pizza.boospace.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    // Get Franchises
+    response = http.get('https://pizza-service.boospace.click/api/franchise', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        dnt: '1',
+        'if-none-match': 'W/"40-EPPawbPn0KtYVCL5qBynMCqA1xo"',
+        origin: 'https://pizza.boospace.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(5)
+
+    // Purchase
+    response = http.post(
+      'https://pizza-service.boospace.click/api/order',
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          dnt: '1',
+          origin: 'https://pizza.boospace.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+          Authorization: `Bearer ${vars['token']}`,
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Purchase was *not* 200');
+    }
+    vars['jwt'] = jsonpath.query(response.json(), '$.jwt')[0]
+    sleep(4)
+
+    // Verify
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      `{"jwt":"${vars['jwt']}"}`,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          dnt: '1',
+          origin: 'https://pizza.boospace.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          Authorization: `Bearer ${vars['token']}`,
+        },
+      }
+    )
+  })
+}
